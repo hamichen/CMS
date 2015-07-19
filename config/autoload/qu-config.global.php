cfa101f857d70756c0510e21a4c0d54242ffb1b7
hami <0912pp123321>
2015-07-19 20:39:54 +0800

    修正 el-finder

--- config/autoload/qu-config.global.php
+++ config/autoload/qu-config.global.php
@@ -3,25 +3,6 @@
 return array(
     'QuConfig'=>array(
         'QuElFinder'=>array(
-            'QuRoots'=>array(
-                'driver'        => 'LocalFileSystem',
-                //Module
-                //'path'  =>  dirname(dirname(dirname(__DIR__)))  . '/web/uploads/files',
-                //Vendor
-                'path'  =>  getcwd()  . '/public/uploads/files',
-                'URL'           =>  '/uploads/files',
-                'accessControl' => 'access',
-                'attributes' => array(
-                    array(
-                        'read'   => false,
-                        'write'  => false,
-                        'locked' => true,
-                        'hidden' => false
-                    )
-                )
-
-            ),
-
             'elFinder'=>array(
                 'url'=>'/quelfinder/connector',
                 'lang'=>   'zh_TW',
@@ -34,8 +15,6 @@ return array(
             'LoadCss'=>array(
 
                 'css'=> array(
-
-                    /*
                     'common',
                     'dialog',
                     'toolbar',
@@ -46,7 +25,7 @@ return array(
                     'quicklook',
                     'commands',
                     'fonts',
-                    */
+
                     'style',
                     'theme',
                     'elfinder.min',
@@ -59,7 +38,7 @@ return array(
                 //'elfinder.min',
 
                 'jquery.elfinder',
-                'elFinder',
+                //'elFinder',
                 'elFinder.version',
                 'elFinder.resources',
                 'elFinder.options',
@@ -121,6 +100,114 @@ return array(
                 'i18n'=> array(
                     'zh_TW'
                 ),
+            ),
+        ),
+
+        'QuCKEditor' => array
+        (
+            'QuElFinderWindowWidth'  => "800",
+            'QuElFinderWindowHeight' => "600",
+
+            'BasePath' =>'/js/plugins/ckeditor', // Require!!
+            'CKEDITOR' => array(
+
+                'filebrowserBrowseUrl'    => '/quelfinder/ckeditor',
+                'filebrowserWindowWidth'  => "1000",
+                'filebrowserWindowHeight' => "600",
+
+                'Width'      => "100%",
+                'Height'     => "340",
+                'language'   => 'ca',
+                'uiColor'=> '#F6F6F6',
+
+                //Producing HTML Compliant Output
+                'coreStyles_bold'   => array('element' => 'b'),
+                'coreStyles_italic' => array('element' => 'i'),
+                'fontSize_style'    => array('element' => 'font','attributes' => array( 'size' => '100px')),
+
+                //MagiCline plugin
+                'magicline_color'=>'blue',
+
+                //Full Page Editing
+                'fullPage' => false,
+                'allowedContent' => false,
+
+                // Toolbars config
+                'toolbar' => null,
+                //'toolbarGroups' => null,
+
+                // Full toolbars
+                /*
+                'toolbar' => array(
+                    array('name'=> 'clipboard',
+                        'groups'=> array( 'clipboard', 'undo' ),
+                        'items' => array( 'Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo' )
+                    ),
+                    array( 'name'=> 'editing',
+                        'groups' => array( 'find', 'selection', 'spellchecker'),
+                        'items' => array( 'Scayt'),
+                    ),
+                    array( 'name'=> 'links',
+                        'items' => array( 'Link', 'Unlink', 'Anchor' ),
+                    ),
+                    array( 'name'=> 'insert',
+                        'items' => array( 'Image', 'Table', 'HorizontalRule', 'SpecialChar'),
+                    ),
+                    array( 'name'=> 'tools',
+                        'items' => array( 'Maximize'),
+                    ),
+                    array( 'name'=> 'document',
+                        'groups' => array( 'mode', 'document', 'doctools' ), 'items' => array('Source')
+                    ),
+                    array( 'name'=> 'others',
+                        'items' => array( '-' ),
+                    ),
+                    '/',
+                    array( 'name'=> 'basicstyles',
+                        'groups' => array( 'basicstyles', 'cleanup' ),
+                        'items' => array( 'Bold', 'Italic', 'Strike', '-', 'RemoveFormat' ),
+                    ),
+                    array( 'name'=> 'paragraph',
+                        'groups' => array( 'list', 'indent', 'blocks', 'align' ),
+                        'items' => array( 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote' ),
+                    ),
+                    array( 'name'=> 'styles',
+                        'items' => array( 'Styles', 'Format' ),
+                    ),
+                    array( 'name'=> 'about',
+                        'items' => array( 'About' ),
+                    ),
+                ),
+                */
+                /*
+                 // Groups toolbars
+                'toolbarGroups' => array(
+                    array( 'name'=> 'clipboard',
+                        'groups'=> array('clipboard', 'undo')
+                    ),
+                    array( 'name'=> 'editing',
+                        'groups'=> array( 'find', 'selection', 'spellchecker')
+                    ),
+                    array( 'name'=> 'links'),
+                    array( 'name'=> 'insert'),
+                    array( 'name'=> 'forms'),
+                    array( 'name'=> 'document',
+                        'groups'=> array( 'find', 'selection', 'spellchecker')
+                    ),
+                    array( 'name'=> 'others'),
+                    '/',
+                    array( 'name'=> 'document',
+                        'groups'=> array( 'basicstyles', 'cleanup')
+                    ),
+                    array( 'name'=> 'paragraph',
+                        'groups'=> array( 'list', 'indent', 'blocks', 'align')
+                    ),
+                    array( 'name'=> 'styles'),
+                    array( 'name'=> 'colors'),
+                    array( 'name'=> 'about')
+                ),
+                */
+
             ),
         ),
 
