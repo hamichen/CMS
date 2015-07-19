be44a7484f66f2a6964144593173a730f8c52903
hami <0912pp123321>
2015-07-19 17:11:47 +0800

    修正 qu-elfinder 設定

--- config/autoload/qu-config.global.php
+++ config/autoload/qu-config.global.php
@@ -2,111 +2,125 @@
 
 return array(
     'QuConfig'=>array(
-        'QuCKEditor' => array
-        (
-            'QuElFinderWindowWidth'  => "800",
-            'QuElFinderWindowHeight' => "600",
+        'QuElFinder'=>array(
+            'QuRoots'=>array(
+                'driver'        => 'LocalFileSystem',
+                //Module
+                //'path'  =>  dirname(dirname(dirname(__DIR__)))  . '/web/uploads/files',
+                //Vendor
+                'path'  =>  getcwd()  . '/public/uploads/files',
+                'URL'           =>  '/uploads/files',
+                'accessControl' => 'access',
+                'attributes' => array(
+                    array(
+                        'read'   => false,
+                        'write'  => false,
+                        'locked' => true,
+                        'hidden' => false
+                    )
+                )
 
-            'BasePath' =>'/js/plugins/ckeditor', // Require!!
-            'CKEDITOR' => array(
+            ),
 
-                'filebrowserBrowseUrl'    => '/quelfinder/ckeditor',
-                'filebrowserWindowWidth'  => "1000",
-                'filebrowserWindowHeight' => "600",
+            'elFinder'=>array(
+                'url'=>'/quelfinder/connector',
+                'lang'=>   'zh_TW',
+                'height'=> '500',
+                'width'=> '500',
+            ),
 
-                'Width'      => "100%",
-                'Height'     => "340",
-                'language'   => 'ca',
-                'uiColor'=> '#F6F6F6',
+            'BasePath'=>'/js/plugins/elfinder',
 
-                //Producing HTML Compliant Output
-                'coreStyles_bold'   => array('element' => 'b'),
-                'coreStyles_italic' => array('element' => 'i'),
-                'fontSize_style'    => array('element' => 'font','attributes' => array( 'size' => '100px')),
+            'LoadCss'=>array(
 
-                //MagiCline plugin
-                'magicline_color'=>'blue',
+                'css'=> array(
 
-                //Full Page Editing
-                'fullPage' => false,
-                'allowedContent' => false,
+                    /*
+                    'common',
+                    'dialog',
+                    'toolbar',
+                    'navbar',
+                    'statusbar',
+                    'contextmenu',
+                    'cwd',
+                    'quicklook',
+                    'commands',
+                    'fonts',
+                    */
+                    'style',
+                    'theme',
+                    'elfinder.min',
 
-                // Toolbars config
-                'toolbar' => null,
-                //'toolbarGroups' => null,
-
-                // Full toolbars
-                /*
-                'toolbar' => array(
-                    array('name'=> 'clipboard',
-                        'groups'=> array( 'clipboard', 'undo' ),
-                        'items' => array( 'Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord', '-', 'Undo', 'Redo' )
-                    ),
-                    array( 'name'=> 'editing',
-                        'groups' => array( 'find', 'selection', 'spellchecker'),
-                        'items' => array( 'Scayt'),
-                    ),
-                    array( 'name'=> 'links',
-                        'items' => array( 'Link', 'Unlink', 'Anchor' ),
-                    ),
-                    array( 'name'=> 'insert',
-                        'items' => array( 'Image', 'Table', 'HorizontalRule', 'SpecialChar'),
-                    ),
-                    array( 'name'=> 'tools',
-                        'items' => array( 'Maximize'),
-                    ),
-                    array( 'name'=> 'document',
-                        'groups' => array( 'mode', 'document', 'doctools' ), 'items' => array('Source')
-                    ),
-                    array( 'name'=> 'others',
-                        'items' => array( '-' ),
-                    ),
-                    '/',
-                    array( 'name'=> 'basicstyles',
-                        'groups' => array( 'basicstyles', 'cleanup' ),
-                        'items' => array( 'Bold', 'Italic', 'Strike', '-', 'RemoveFormat' ),
-                    ),
-                    array( 'name'=> 'paragraph',
-                        'groups' => array( 'list', 'indent', 'blocks', 'align' ),
-                        'items' => array( 'NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'Blockquote' ),
-                    ),
-                    array( 'name'=> 'styles',
-                        'items' => array( 'Styles', 'Format' ),
-                    ),
-                    array( 'name'=> 'about',
-                        'items' => array( 'About' ),
-                    ),
                 ),
-                */
-                /*
-                 // Groups toolbars
-                'toolbarGroups' => array(
-                    array( 'name'=> 'clipboard',
-                        'groups'=> array('clipboard', 'undo')
-                    ),
-                    array( 'name'=> 'editing',
-                        'groups'=> array( 'find', 'selection', 'spellchecker')
-                    ),
-                    array( 'name'=> 'links'),
-                    array( 'name'=> 'insert'),
-                    array( 'name'=> 'forms'),
-                    array( 'name'=> 'document',
-                        'groups'=> array( 'find', 'selection', 'spellchecker')
-                    ),
-                    array( 'name'=> 'others'),
-                    '/',
-                    array( 'name'=> 'document',
-                        'groups'=> array( 'basicstyles', 'cleanup')
-                    ),
-                    array( 'name'=> 'paragraph',
-                        'groups'=> array( 'list', 'indent', 'blocks', 'align')
-                    ),
-                    array( 'name'=> 'styles'),
-                    array( 'name'=> 'colors'),
-                    array( 'name'=> 'about')
-                ),
-                */
+            ),
 
+            'LoadJs'=>array(
+
+                //'elfinder.min',
+
+                'jquery.elfinder',
+                'elFinder',
+                'elFinder.version',
+                'elFinder.resources',
+                'elFinder.options',
+                'elFinder.history',
+                'elFinder.command',
+
+
+                'ui'=> array(
+                    'overlay',
+                    'workzone',
+                    'navbar',
+                    'dialog',
+                    'tree',
+                    'cwd',
+                    'toolbar',
+                    'button',
+                    'uploadButton',
+                    'viewbutton',
+                    'searchbutton',
+                    'sortbutton',
+                    'panel',
+                    'contextmenu',
+                    'path',
+                    'stat',
+                    'places',
+                ),
+
+                'commands'=> array(
+                    'open',
+                    'reload',
+                    'home',
+                    'up',
+                    'back',
+                    'forward',
+                    'getfile',
+                    'quicklook',
+                    'download',
+                    'rm',
+                    'duplicate',
+                    'rename',
+                    'mkdir',
+                    'mkfile',
+                    'upload',
+                    'copy',
+                    'cut',
+                    'paste',
+                    'edit',
+                    'extract',
+                    'archive',
+                    'search',
+                    'info',
+                    'view',
+                    'help',
+                    'resize',
+                    'sort',
+                    'netmount'
+                ),
+
+                'i18n'=> array(
+                    'zh_TW'
+                ),
             ),
         ),
 
