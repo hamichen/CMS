2b73b0cc164dd896fd4bea0b4f13aed6213cfa36
hami <0912pp123321>
2015-07-19 15:09:35 +0800

    更新 CMS 程式

--- module/Base/src/Base/Qualified/NS/LogFactory.php
+++ module/Base/src/Base/Qualified/NS/LogFactory.php
@@ -9,16 +9,13 @@ class LogFactory implements FactoryInterface
 {
     public function createService(ServiceLocatorInterface $serviceLocator)
     {
-        $session = new Container('school');
-
-        $school = $session->school;
 
         $logPath = './data/log';
         if (!is_dir($logPath)){
             mkdir($logPath, 0777, true);
         }
         $log = new \Zend\Log\Logger();
-        $writer = new \Zend\Log\Writer\Stream($logPath.'/'.$school['edu_no'].'.log');
+        $writer = new \Zend\Log\Writer\Stream($logPath.'/'.'system.log');
         $log->addWriter($writer);
         return $log;
     }
