8dbc12470c2dbe7e58710366afaa86e9653299be
root <root@localhost.localdomain>
2019-01-23 10:02:46 +0800

    api

--- module/Admin/src/Controller/ApiController.php
+++ module/Admin/src/Controller/ApiController.php
@@ -12,18 +12,32 @@ namespace Admin\Controller;
 use Application\TcApi\SemesterData;
 use Application\TcApi\TcApi;
 use Base\Controller\BaseController;
+use Zend\View\Model\JsonModel;
 
 class ApiController extends BaseController
 {
     public function indexAction()
     {
+
+    }
+
+    public function syncSemesterAction()
+    {
         // 取得 serviceManager
         $sm = $this->getServiceManager();
         // 建立 APi 物件
         $api = new SemesterData($sm);
+        $jsonModel = new JsonModel();
+        try {
+            $api->syncData();
+            $jsonModel->setVariable('success', true);
+        }
+        catch (\Exception $e) {
+            $jsonModel->setVariable('success', false);
+            $jsonModel->setVariable('message', $e->getMessage());
+        }
 
-        $api->syncData();
-        exit;
+        return $jsonModel;
     }
 
 }
